9f4f1101f170a58750f8bd7b924b5a11320ac057
AikoCute <aikocute@icloud.com>
2022-11-16 14:23:34 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -97,7 +97,7 @@ check_status() {
 }
 
 install_acme() {
-    curl https://raw.githubusercontent.com/Github-Aiko/AikoR-Install/master/file/acme.sh | sh
+    curl https://raw.githubusercontent.com/AikoCute-Offical/AikoR-Install/master/file/acme.sh | sh
 }
 
 install_AikoR() {
